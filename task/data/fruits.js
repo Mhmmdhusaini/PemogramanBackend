99145a8921d748fcf276e9fdf16e51bea27d76b1
Mhmmdhusaini <husaini20205@gmail.com>
2024-12-08 22:39:07 +0700

    tugas pertemuan 9

---
+++ task/data/fruits.js
@@ -0,0 +1,5 @@
+// TODO 1: Buat array yg berisi data buah, Refactor variable ke ES6 Variable.
+const fruits = ["Jeruk", "Apel", "Durian"];
+
+// TODO 2: Export variable fruits
+ export default fruits;
